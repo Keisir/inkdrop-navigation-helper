c5910dcefd0a72ec1ff26399cdb7760415fa4059
Lukas <100614278+Keisir@users.noreply.github.com>
2023-05-02 17:07:53 +0200

    added the ui component

---
+++ src/navigation-component.tsx
@@ -0,0 +1,30 @@
+'use babel';
+
+import React from 'react';
+
+export class NavigationComponent extends React.Component {
+    render() {
+        return (
+            <div className="sidebar-menu-item navigation-helper-container">
+                <button
+                    className="navigation-helper-item"
+                    title="Back"
+                    onClick={() => {
+                        inkdrop.commands.dispatch(document.body, 'core:navigate-back');
+                    }}
+                >
+                    <i aria-hidden="true" className="chevron left icon" />
+                </button>
+                <button
+                    className="navigation-helper-item"
+                    title="Forward"
+                    onClick={() => {
+                        inkdrop.commands.dispatch(document.body, 'core:navigate-forward');
+                    }}
+                >
+                    <i aria-hidden="true" className="chevron right icon" />
+                </button>
+            </div>
+        );
+    }
+}
--- src/navigation-helper.ts
+++ src/navigation-helper.ts
@@ -1,10 +1,15 @@
 'use babel';
 
+import { NavigationComponent } from './navigation-component';
+
 export {};
 
 class NavigationHelperPlugin {
     activate = async () => {
         window.addEventListener('mouseup', this.navigationHandler);
+
+        inkdrop.components.registerClass(NavigationComponent);
+        inkdrop.layouts.insertComponentToLayoutBefore('sidebar-menu', 'SideBarMenuItemAllNotes', NavigationComponent.name);
     };
 
     navigationHandler = (event: MouseEvent) => {
@@ -18,6 +23,9 @@ class NavigationHelperPlugin {
 
     deactivate = async () => {
         window.removeEventListener('mouseup', this.navigationHandler);
+
+        inkdrop.layouts.removeComponentFromLayout('sidebar-menu', NavigationComponent.name);
+        inkdrop.components.deleteClass(NavigationComponent);
     };
 }
 
